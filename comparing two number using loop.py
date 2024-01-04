e86d2738b251023bc2a918a9cca77f8711aea23f
logapathan <75413426+logapathan@users.noreply.github.com>
2024-01-04 18:15:50 +0530

    Add files via upload

---
+++ comparing two number using loop.py
@@ -0,0 +1,13 @@
+
+for num1 in range(-5,5,1):
+    for num2 in range(-5,5,1):
+        if num1 < num2:
+            print(f"{num1} is Lesser than {num2}")
+        elif num1 > num2:
+            print(f"{num1} is Greater than {num2}")
+        else:
+            print(f"{num1} is Equal to {num2}")
+
+
+
+
