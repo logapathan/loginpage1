428341c48ea60de8378307eae3f8e88d2a776e17
logapathan <75413426+logapathan@users.noreply.github.com>
2024-01-04 18:13:39 +0530

    Add files via upload

---
+++ eligibletovoteornot.py
@@ -0,0 +1,10 @@
+name=input("Enter Your Name:")
+age=int(input("Enter Your Age:"))
+voter_id=input("Enter Your voter id:")
+city=input("Enter Your City:")
+print("************************************************************")
+if age<18:
+    print("Sorry,You are not eligible to vote")
+else:
+    print("You are eligible to vote")
+    
